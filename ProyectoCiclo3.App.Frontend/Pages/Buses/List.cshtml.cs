f930e94051470ae0033e47c9e9b2e4fda6ded4cf
Jonathan01701 <112448413+Jonathan01701@users.noreply.github.com>
2022-09-03 20:44:38 -0500

    Actualizacion List.cshtml.cs

--- ProyectoCiclo3.App.Frontend/Pages/Buses/List.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Buses/List.cshtml.cs
@@ -1,17 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using ProyectoCiclo3.App.Dominio;
+ 
 namespace ProyectoCiclo3.App.Frontend.Pages
 {
-    public class ListBusesModel : PageModel
+    public class ListBusModel : PageModel
     {
-        public void OnGet()
-        {
-
-        }
+       
+        private readonly RepositorioBuses repositorioBuses;
+        public IEnumerable<Buses> Buses {get;set;}
+ 
+    public ListBusModel(RepositorioBuses repositorioBuses)
+    {
+        this.repositorioBuses=repositorioBuses;
+     }
+ 
+    public void OnGet()
+    {
+        Buses=repositorioBuses.GetAll();
+    }
     }
 }
