da2f711b80c22d96d7e8d4d48a3e993608c73058
Jonathan01701 <112448413+Jonathan01701@users.noreply.github.com>
2022-09-04 22:56:48 -0500

    Adicion de codigo
    Se adiciona codigo segun lo expuesto en la clase

--- ProyectoCiclo3.App.Frontend/Pages/Buses/List.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Buses/List.cshtml.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
 using ProyectoCiclo3.App.Dominio;
  
 namespace ProyectoCiclo3.App.Frontend.Pages
