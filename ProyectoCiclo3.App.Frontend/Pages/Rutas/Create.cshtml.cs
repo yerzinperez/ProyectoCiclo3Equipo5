664c6d4a9b9ac4369a16571972aa5ccbb9a17efc
Yerzin Perez <yerzin24@hotmail.com>
2022-09-12 20:57:57 -0500

    Se intenta validar el back con el front de rutas

--- ProyectoCiclo3.App.Frontend/Pages/Rutas/Create.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Rutas/Create.cshtml.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
+using ProyectoCiclo3.App.Dominio;
 
-namespace ProyectoCiclo3.App.Frontend.Pages
-{
-    public class FormRutasModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace ProyectoCiclo3.App.Frontend.Pages {
 
+    public class FormRutasModel : PageModel {
+
+        private readonly RepositorioEstaciones repositorioEstaciones;
+        private readonly RepositorioRutas repositorioRutas;
+        public IEnumerable<Estaciones> Estaciones{get;set;}
+        public Rutas Rutas { get;set;}
+
+        public FormRutasModel(RepositorioEstaciones repositorioEstaciones, RepositorioRutas repositorioRutas){
+            this.repositorioEstaciones = repositorioEstaciones;
+            this.repositorioRutas= repositorioRutas;
+        }
+
+        public void OnGet(){
+            Estaciones = repositorioEstaciones.GetAll();
+            Rutas = repositorioRutas.GetAll();
+        }
+
+        public IActionResult OnPost(){
+            if(!ModelState.IsValid){
+                return Page();
+            }            
+            Rutas = repositorioRutas.Create(Rutas);            
+            return RedirectToPage("./List");
         }
     }
 }
--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioRutas.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioRutas.cs
@@ -3,39 +3,35 @@ using ProyectoCiclo3.App.Dominio;
 using System.Linq;
 using System;
  
-namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
-{
-    public class RepositorioRutas
-    {
+namespace ProyectoCiclo3.App.Persistencia.AppRepositorios{
+
+    public class RepositorioRutas{
+
         List<Rutas> rutas;
+        private readonly AppContext _appContext = new AppContext();
  
-    public RepositorioRutas()
-        {
-            rutas= new List<Rutas>()
-            {
-                new Rutas{id=1,tiempo_estimado= 14},
-                new Rutas{id=2,tiempo_estimado= 9},
-                new Rutas{id=3,tiempo_estimado= 15}
-            };
-        }
+        public RepositorioRutas(){}
  
         public IEnumerable<Rutas> GetAll()
         {
-            return rutas;
+            return _appContext.Rutas;
         }
  
         public Rutas GetWithId(int id){
-            return rutas.SingleOrDefault(e => e.id == id);
+            return _appContext.Rutas.Find(id);
         }
 
-        public Rutas Delete(int id)
-        {
-        var ruta= rutas.SingleOrDefault(b => b.id == id);
-        rutas.Remove(ruta);
-        return ruta;
+        public Rutas Create(Rutas newRuta){
+            var addRuta = _appContext.Rutas.Add(newRuta);
+            _appContext.SaveChanges();
+            return addRuta.Entity;
         }
 
 
-
+        public Rutas Delete(int id){
+            var ruta= rutas.SingleOrDefault(b => b.id == id);
+            rutas.Remove(ruta);
+            return ruta;
+            }
     }
 }
